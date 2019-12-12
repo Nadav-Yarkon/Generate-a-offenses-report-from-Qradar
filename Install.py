94b07d165cc60cc0e45a175f13001e165b38bd6f
Nadav-Yarkon <31694155+Nadav-Yarkon@users.noreply.github.com>
2019-12-12 23:59:11 +0200

    Update Install.py

--- Install.py
+++ Install.py
@@ -1,9 +1,9 @@
 import os
 
 print(Installing now all library)
-os.system("pip inatall datetime")
-os.system("pip inatall python-docx")
-os.system("pip inatall numpy")
-os.system("pip inatall matplotlib")
-os.system("pip inatall requests")
-os.system("pip inatall urllib3")
+os.system("pip install datetime")
+os.system("pip install python-docx")
+os.system("pip install numpy")
+os.system("pip install matplotlib")
+os.system("pip install requests")
+os.system("pip install urllib3")
